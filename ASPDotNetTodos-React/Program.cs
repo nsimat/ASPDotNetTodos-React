7a66bf6cc0601e03cf6d53d8bca1d55fa9e0d82c
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2022-06-21 01:03:53 +0200

    Adding OpenAPI/Swagger to the project

--- ASPDotNetTodos-React/Program.cs
+++ ASPDotNetTodos-React/Program.cs
@@ -2,13 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+//builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+
+
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new() { Title = "Todo List API", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseStaticFiles();
