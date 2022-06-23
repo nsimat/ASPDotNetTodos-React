d53eb6b1d75467d2bb05e94868c38c8369253689
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2022-06-23 23:51:58 +0200

    Implementing TodoFormComponent

--- ASPDotNetTodos-React/ClientApp/src/components/TodoFormComponent.jsx
+++ ASPDotNetTodos-React/ClientApp/src/components/TodoFormComponent.jsx
@@ -1,1 +1,44 @@
-﻿
+﻿import React from 'react';
+import { Form, Row, Col, Button, Input } from 'antd';
+import { PlusCircleFilled } from '@antd-design/icons';
+
+function TodoFormComponent({ onFormSubmit }) {
+    const [form] = Form.useForm();
+
+    const onFinish = () => {
+        onFormSubmit({
+            title: form.getFieldValue('title'),
+            completed: false
+        });
+        console.log(form.getFieldValue('title'));
+
+        form.resetFields();
+    }
+
+    return (
+        <Form
+            form={form}
+            onFinish={onFinish()}
+            layout="horizontal"
+            className="todo-form">
+            <Row gutter={20}>
+                <Col xs={24} sm={24} md={17} lg={19} xl={20}>
+                    <Form.Item
+                        name={'title'}
+                        rules={[{ required: true, message: 'This field is required.' }]}>
+                        <Input placeholder="What needs to be done?"/>
+                    </Form.Item>
+                </Col>
+                <Col xs={24} sm={24} md={7} lg={5} xl={4}>
+                    <Button type="primary" htmltype="submit" block>
+                        <PlusCircleFilled/>
+                        Add Todo
+                    </Button>
+                </Col>
+            </Row>
+
+        </Form>
+    );
+}
+
+export default TodoFormComponent();
