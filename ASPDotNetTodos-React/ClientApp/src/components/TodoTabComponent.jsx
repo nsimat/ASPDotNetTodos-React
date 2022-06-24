725edf9db0dd202b7891f17b9be1d46a7fc7d76a
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2022-06-24 17:59:24 +0200

    Implementing and validating TodoTabComponent

--- ASPDotNetTodos-React/ClientApp/src/components/TodoTabComponent.jsx
+++ ASPDotNetTodos-React/ClientApp/src/components/TodoTabComponent.jsx
@@ -1,1 +1,27 @@
-﻿
+﻿import React, { useEffect } from 'react';
+import { Tabs, Layout, Row, List } from 'antd';
+import TodoItemComponent from './TodoItemComponent';
+
+function TodoTabComponent({ todos, onTodoRemoval, onTodoToggle }) {
+    return (
+        <>
+            <List>
+                locale{{ emptyText: "There's nothing to do :(", }}
+                dataSource={todos}
+                renderIten={(todo) => {
+                    <TodoItemComponent
+                        todo={todo}
+                        onTodoToggle={onTodoToggle}
+                        onTodoRemoval={onTodoRemoval}
+                    />
+                }}
+                pagination={{
+                    position: 'bottom',
+                    pageSize: 10,
+                }}
+            </List>
+        </>
+    );
+}
+
+export default TodoTabComponent;
