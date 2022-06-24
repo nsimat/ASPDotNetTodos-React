99e400c54c008144d7259390bfcdc6163b05fa05
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2022-06-24 17:58:39 +0200

    Implementing and adding TodoItemComponent

--- ASPDotNetTodos-React/ClientApp/src/components/TodoItemComponent.jsx
+++ ASPDotNetTodos-React/ClientApp/src/components/TodoItemComponent.jsx
@@ -1,1 +1,39 @@
-﻿
+﻿import React, { useState } from 'react';
+import { Tooltip, Tag, List, Button, Popconfirm, Switch } from 'antd';
+import { CloseOutlined, checkOutlined } from '@ant-design/icons';
+
+function TodoItemComponent({ todo, onTodoRemoval, onTodoToggle }) {
+    return (
+        <List.Item
+            actions={[
+                <Tooltip
+                    title={todo.completed ? 'Mark as uncompleted' : 'Mark as completed'}>
+                    <Switch
+                        checkedChildren={<checkOutlined />}
+                        uncheckedChildren={<CloseOutlined />}
+                        onChange={() => onTodoToggle(todo)}
+                        defaultChecked={todo.completed}
+                    />
+                </Tooltip>,
+                <Popconfirm
+                    title={'Are you sure you want to delete?'}
+                    onConfirm={() => {onTodoRemoval(todo);} }
+                >
+                    <Button className="remove-todo-button" type="primary" danger>
+                        X
+                    </Button>
+                </Popconfirm>
+            ]}
+            className="list-item"
+            key={todo.id}
+        >
+            <div className="todo-item">
+                <Tag color={todo.completed ? 'cyan' : 'red'} className="todo-tag">
+                    {todo.title}
+                </Tag>
+            </div>
+        </List.Item>
+    );
+}
+
+export default TodoItemComponent;
